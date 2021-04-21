47f3b93ab4ab6a66ce006e41dcf16467fb74297f
Claudia Schoenherr <mai-claudia.schoenherr@stud.uni-regensburg.de>
2021-04-21 15:05:29 +0200

    Add Calculator Layout

--- calculator.py
+++ calculator.py
@@ -1,1 +1,9 @@
-# your implementation here
+import sys
+from PyQt5 import uic, Qt
+
+if __name__ == "__main__":
+    app = Qt.QApplication(sys.argv)
+    win = uic.loadUi("calculator.ui")
+
+    win.show()
+    app.exec()
